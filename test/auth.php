f4d816ea6e6f1a773af2b21db55cc25279f69f45
caphun <caphun@tofugear.com>
2013-01-10 00:29:37 +0800

    added empty auth tests

--- test/auth.php
+++ test/auth.php
@@ -5,15 +5,65 @@ require_once '../lib/ably.php';
 
 class AuthTest extends PHPUnit_Framework_TestCase {
 
-    protected $app;
+    protected $defaults;
 
     protected function setUp() {
-        $this->app = new Ably(array(
-            'host' => defined('ABLY_HOST') ? ABLY_HOST : '',
-            'key'  => ABLY_KEY,
-            'debug' => true
-        ));
+
+        $defaults = array( 'host' => getenv("WEBSOCKET_ADDRESS"), 'debug' => true );
+
+        if (empty($defaults['host'])) {
+            $defaults['host'] = "staging-rest.ably.io";
+            $defaults['encrypted'] = true;
+        } else {
+            $defaults['encrypted'] = $defaults['host'] != "localhost";
+            $defaults['port'] = $defaults['encrypted'] ? 8081 : 8080;
+        }
+
+        $this->defaults = $defaults;
     }
+
+
+    /**
+     * Init library with a key only
+     */
+    public function testAuthoriseWithKeyOnly() {
+        echo '== testAuthoriseWithKeyOnly()';
+    }
+
+
+    /**
+     * Init library with a token only
+     */
+    public function testAuthoriseWithTokenOnly() {
+        echo '== testAuthoriseWithTokenOnly()';
+    }
+
+    /**
+     * Init library with a token callback
+     */
+    public function testAuthoriseWithTokenCallback() {
+        echo '== testAuthoriseWithTokenCallback()';
+    }
+
+
+    /**
+     * Init library with a key and clientId; expect token auth to be chosen
+     */
+    public function testAuthoriseWithKeyAndClientId() {
+        echo '== testAuthoriseWithKeyAndClientId()';
+    }
+
+    /**
+     * Init library with a token
+     */
+    public function testAuthoriseWithToken() {
+        echo '== testAuthoriseWithKeyOnly()';
+    }
+
+
+    /*
+     * OLD TESTS - DEPRECATED
+     */
 
     public function testAuthoriseWithSignedToken() {
         echo '== testAuthoriseWithSignedToken()';
