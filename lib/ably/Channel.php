2512d5a49d47fbe221b0edf1dbf87827336bb77c
caphun <caphun@tofugear.com>
2013-01-15 15:36:17 +0800

    code refactor

--- lib/ably/Channel.php
+++ lib/ably/Channel.php
@@ -20,38 +20,41 @@ class Channel {
      * Public methods
      */
         public function history( $params = array() ) {
-            return $this->get_resource( '/history', $params );
+            return $this->get( '/history', $params );
         }
 
         public function presence( $params = array() ) {
-            return $this->get_resource( '/presence', $params );
+            return $this->get( '/presence', $params );
         }
 
         public function presence_history( $params = array() ) {
-            return $this->get_resource( '/presence/history', $params );
+            return $this->get( '/presence/history', $params );
         }
 
         public function publish( $name, $data ) {
-            $this->ably->log_action( 'Channel.publish()', 'name = '. $name );
-            return $this->post_resource( '/publish', array( 'name' => $name, 'data' => $data ) );
+            $this->log_action( 'Channel.publish()', 'name = '. $name );
+            return $this->post( '/publish', array( 'name' => $name, 'data' => $data ) );
         }
 
         public function stats( $params = array() ) {
-            return $this->get_resource( '/stats', $params );
+            return $this->get( '/stats', $params );
         }
 
 
     /*
      * Private methods
      */
-        private function get_resource( $path, $params = array() ) {
+        private function get( $path, $params = array() ) {
             $this->ably->authorise();
             return $this->ably->get( $this->domain, $path, $this->ably->auth_headers(), $params );
         }
 
-        private function post_resource( $path, $params = array() ) {
+        private function log_action( $action, $msg ) {
+            $this->ably->log_action( $action, $msg );
+        }
+
+        private function post( $path, $params = array() ) {
             $this->ably->authorise();
             return $this->ably->post( $this->domain, $path, $this->ably->auth_headers(), $params );
         }
-
 }
