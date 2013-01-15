2128fe6c3ea49d75e4f535aa6752497286fe518e
caphun <caphun@tofugear.com>
2013-01-16 01:49:20 +0800

    added test placeholders

--- test/InitTest.php
+++ test/InitTest.php
@@ -4,5 +4,43 @@ require_once '../lib/ably.php';
 require_once 'factories/TestOption.php';
 
 class InitTest extends PHPUnit_Framework_TestCase {
+    /**
+     * Init library with a key only
+     */
 
+    /**
+     * Init library with a key in options
+     */
+
+    /**
+     * Init library with appId
+     */
+
+    /**
+     * Verify library fails to init when both appId and key are missing
+     */
+
+    /**
+     * Init library with specified host
+     */
+
+    /**
+     * Init library with specified port
+     */
+
+    /**
+     * Verify encrypted defaults to true
+     */
+
+    /**
+     * Verify encrypted can be set to false
+     */
+
+    /**
+     * Init with log handler; check called
+     */
+
+    /**
+     * Init with log handler; check not called if logLevel == NONE
+     */
 }
--- test/TokenTest.php
+++ test/TokenTest.php
@@ -4,5 +4,55 @@ require_once '../lib/ably.php';
 require_once 'factories/TestOption.php';
 
 class TokenTest extends PHPUnit_Framework_TestCase {
+    /**
+     * Base requestToken case with null params
+     */
 
+    /**
+     * Base requestToken case with non-null but empty params
+     */
+
+    /**
+     * requestToken with explicit timestamp
+     */
+
+    /**
+     * requestToken with explicit, invalid timestamp
+     */
+
+    /**
+     * requestToken with system timestamp
+     */
+
+    /**
+     * requestToken with duplicate nonce
+     */
+
+    /**
+     * Base requestToken case with non-null but empty params
+     */
+
+    /**
+     * Token generation with capability that subsets key capability
+     */
+
+    /**
+     * Token generation with specified key
+     */
+
+    /**
+     * requestToken with invalid mac
+     */
+
+    /**
+     * Token generation with specified ttl
+     */
+
+    /**
+     * Token generation with excessive ttl
+     */
+
+    /**
+     * Token generation with invalid ttl
+     */
 }
