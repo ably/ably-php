c8391e093d72acb766b428f84978733c97e51f10
caphun <caphun@tofugear.com>
2013-01-16 10:55:42 +0800

    completed channel history tests

--- test/ChannelHistoryTest.php
+++ test/ChannelHistoryTest.php
@@ -194,9 +194,96 @@ class ChannelHistoryTest extends PHPUnit_Framework_TestCase {
     /**
      * Publish events and check expected history based on time slice (forwards)
      */
+    public function testPublishEventsAndCheckExpectedHistoryInTimeSlicesForwards() {
+        echo '==testPublishEventsAndCheckExpectedHistoryInTimeSlicesForwards()';
+
+        $interval_start = $interval_end = 0;
+
+        # first publish some messages
+        $history5 = $this->ably->channel('history5');
+
+        # send batches of messages with short inter-message delay
+        for ($i=0; $i<20; $i++) {
+            $history5->publish('history'.$i, $i);
+            usleep(100000); // sleep for 0.1 of a second
+        }
+        $interval_start = intval(microtime(true)*1000);
+        for ($i=20; $i<40; $i++) {
+            $history5->publish('history'.$i, $i);
+            usleep(100000); // sleep for 0.1 of a second
+        }
+        $interval_end = intval(microtime(true)*1000) - 1;
+        for ($i=20; $i<40; $i++) {
+            $history5->publish('history'.$i, $i);
+            usleep(100000); // sleep for 0.1 of a second
+        }
+
+        # wait for the history to be persisted
+        sleep(10);
+
+        # get the history for this channel
+        $messages = $history5->history(array(
+            'direction' => 'forwards',
+            'start'     => $interval_start,
+            'end'       => $interval_end,
+        ));
+        $this->assertNotNull( $messages, 'Expect non-null messages' );
+        $this->assertEquals( 20, count($messages), 'Expected 20 messages' );
+
+        # verify message order
+        $actual_message_history = array();
+        for ($i=20; $i<40; $i++) {
+            array_push( $actual_message_history, $messages[$i]->data);
+        }
+        $expected_message_history = range(20, 39);
+        $this->assertEquals( $expected_message_history, $actual_message_history, 'Expect messages in forward order');
+    }
 
     /**
      * Publish events and check expected history based on time slice (backwards)
      */
+    public function testPublishEventsAndCheckExpectedHistoryInTimeSlicesBackwards() {
+        echo '==testPublishEventsAndCheckExpectedHistoryInTimeSlicesBackwards()';
 
+        $interval_start = $interval_end = 0;
+
+        # first publish some messages
+        $history6 = $this->ably->channel('history6');
+
+        # send batches of messages with short inter-message delay
+        for ($i=0; $i<20; $i++) {
+            $history6->publish('history'.$i, $i);
+            usleep(100000); // sleep for 0.1 of a second
+        }
+        $interval_start = intval(microtime(true)*1000);
+        for ($i=20; $i<40; $i++) {
+            $history6->publish('history'.$i, $i);
+            usleep(100000); // sleep for 0.1 of a second
+        }
+        $interval_end = intval(microtime(true)*1000) - 1;
+        for ($i=20; $i<40; $i++) {
+            $history6->publish('history'.$i, $i);
+            usleep(100000); // sleep for 0.1 of a second
+        }
+
+        # wait for the history to be persisted
+        sleep(10);
+
+        # get the history for this channel
+        $messages = $history6->history(array(
+            'direction' => 'backwards',
+            'start'     => $interval_start,
+            'end'       => $interval_end,
+        ));
+        $this->assertNotNull( $messages, 'Expect non-null messages' );
+        $this->assertEquals( 20, count($messages), 'Expected 20 messages' );
+
+        # verify message order
+        $actual_message_history = array();
+        for ($i=20; $i<40; $i++) {
+            array_push( $actual_message_history, $messages[$i]->data);
+        }
+        $expected_message_history = range(39, 20, -1);
+        $this->assertEquals( $expected_message_history, $actual_message_history, 'Expect messages in backward order' );
+    }
 }
