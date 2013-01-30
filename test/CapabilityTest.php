c1c241f1881241c8dcc62a97ba9f0836b5361753
caphun <caphun@tofugear.com>
2013-01-31 04:21:38 +0800

    completed capability tests

--- test/CapabilityTest.php
+++ test/CapabilityTest.php
@@ -36,42 +36,253 @@ class CapabilityTest extends PHPUnit_Framework_TestCase {
      * Blanket intersection with specified key
      */
     public function testBlanketIntersectionWithSpecifiedKey() {
-        $token_details = $this->ably->request_token();
+        $key = self::$options['keys'][1];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $token_details = $this->ably->request_token($auth_options, null);
         $this->assertNotNull($token_details->id, 'Expected token id');
-        $this->assertEquals(self::$options['capability'], $token_details->capability, 'Unexpected capability');
+        $this->assertEquals($key->capability, $token_details->capability, 'Unexpected capability');
     }
 
     /**
      * Equal intersection with specified key
      */
     public function testEqualIntersectionWithSpecifiedKey() {
-        $token_details = $this->ably->request_token();
+        $key = self::$options['keys'][1];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $token_params = array(
+            'capability' => $key->capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
         $this->assertNotNull($token_details.id, 'Expected token id');
-        $this->assertEquals(self::$options['capability'], $token_details->capability, 'Unexpected capability');
+        $this->assertEquals($key->capability, $token_details->capability, 'Unexpected capability');
     }
 
     /**
      * Empty ops intersection
      */
+    public function testEmptyOpsIntersection() {
+        echo '==testEmptyOpsIntersection()';
+        $key = self::$options['keys'][1];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $capability = array( 'testchannel' => array('subscribe') );
+        $token_params = array(
+            'capability' => $capability,
+        );
+        try {
+            $token_details = $this->ably->request_token($auth_options, $token_params);
+            $this->fail('Invalid capability, expected rejection');
+        } catch (Exception $e) {
+            $this->assertEquals( 40160, $e->getCode(), 'Unexpected error code' );
+        }
+    }
 
     /**
      * Empty paths intersection
      */
+    public function testEmptyPathIntersection() {
+        echo '==testEmptyPathIntersection()';
+        $key = self::$options['keys'][1];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $capability = array( 'testchannelx' => array('publish') );
+        $token_params = array(
+            'capability' => $capability,
+        );
+        try {
+            $token_details = $this->ably->request_token($auth_options, $token_params);
+            $this->fail('Invalid capability, expected rejection');
+        } catch (Exception $e) {
+            $this->assertEquals( 40160, $e->getCode(), 'Unexpected error code' );
+        }
+    }
 
     /**
      * Non-empty ops intersection
      */
+    public function testNonEmptyOpsIntersection() {
+        echo '==testNonEmptyOpsIntersection()';
+        $key = self::$options['keys'][4];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $requested_capability = array( 'channel2' => array('presence', 'subscribe') );
+        $expected_capability = array( 'channel2' => array('subscribe') );
+        $token_params = array(
+            'capability' => $requested_capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
+        $this->assertNotNull( $token_details->id, 'Expected token id' );
+        $this->assertEquals( json_encode($expected_capability), $token_details->capability, 'Unexpected capability' );
+    }
 
     /**
      * Non-empty paths intersection
      */
+    public function testNonEmptyPathsIntersection() {
+        echo '==testNonEmptyPathsIntersection()';
+        $key = self::$options['keys'][4];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $requested_capability = array(
+            'channel2' => array('presence', 'subscribe'),
+            'channelx' => array('presence', 'subscribe'),
+        );
+        $expected_capability = array(
+            'channel2' => array('subscribe'),
+        );
+        $token_params = array(
+            'capability' => $requested_capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
+        $this->assertNotNull( $token_details->id, 'Expected token id' );
+        $this->assertEquals( json_encode($expected_capability), $token_details->capability, 'Unexpected capability' );
+    }
 
     /**
      * Wildcard ops intersection
      */
+    public function testWildcardOpsIntersection0() {
+        echo '==testWildcardOpsIntersection0()';
+        $key = self::$options['keys'][4];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $requested_capability = array( 'channel2' => array('*') );
+        $expected_capability = array( 'channel2' => array('publish', 'subscribe') );
+        $token_params = array(
+            'capability' => $requested_capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
+        $this->assertNotNull( $token_details->id, 'Expected token id' );
+        $this->assertEquals( json_encode($expected_capability), $token_details->capability, 'Unexpected capability' );
+    }
+
+    public function testWildcardOpsIntersection1() {
+        echo '==testWildcardOpsIntersection1()';
+        $key = self::$options['keys'][4];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $requested_capability = array( 'channel6' => array('publish', 'subscribe') );
+        $expected_capability = array( 'channel6' => array('publish', 'subscribe') );
+        $token_params = array(
+            'capability' => $requested_capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
+        $this->assertNotNull( $token_details->id, 'Expected token id' );
+        $this->assertEquals( json_encode($expected_capability), $token_details->capability, 'Unexpected capability' );
+    }
 
     /**
      * Wildcard resources intersection
      */
+    public function testWildcardResourcesIntersection0() {
+        echo '==testWildcardResourcesIntersection0()';
+        $key = self::$options['keys'][2];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $requested_capability = array( 'cansubscribe' => array('subscribe') );
+        $token_params = array(
+            'capability' => $requested_capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
+        $this->assertNotNull( $token_details->id, 'Expected token id' );
+        $this->assertEquals( json_encode($requested_capability), $token_details->capability, 'Unexpected capability' );
+    }
+
+    public function testWildcardResourcesIntersection1() {
+        echo '==testWildcardResourcesIntersection1()';
+        $key = self::$options['keys'][2];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $requested_capability = array( 'canpublish:check' => array('publish') );
+        $token_params = array(
+            'capability' => $requested_capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
+        $this->assertNotNull( $token_details->id, 'Expected token id' );
+        $this->assertEquals( json_encode($requested_capability), $token_details->capability, 'Unexpected capability' );
+    }
+
+    public function testWildcardResourcesIntersection2() {
+        echo '==testWildcardResourcesIntersection2()';
+        $key = self::$options['keys'][2];
+        $auth_options = array(
+            'keyId' => $key->key_id,
+            'keyValue' => $key->key_value,
+        );
+        $requested_capability = array( 'cansubscribe:*' => array('subscribe') );
+        $token_params = array(
+            'capability' => $requested_capability,
+        );
+        $token_details = $this->ably->request_token($auth_options, $token_params);
+        $this->assertNotNull( $token_details->id, 'Expected token id' );
+        $this->assertEquals( json_encode($requested_capability), $token_details->capability, 'Unexpected capability' );
+    }
+
+    /**
+     * Invalid capabilities
+     */
+    public function testInvalidCapabilities0() {
+        echo '==testInvalidCapabilities0()';
+        $invalid_capability = array( 'channel0' => array('publish_') );
+        $token_params = array(
+            'capability' => $invalid_capability,
+        );
+        try {
+            $this->ably->request_token(null, $token_params);
+            $this->fail('Invalid capability, expected rejection');
+        } catch (Exception $e) {
+            $this->assertEquals( 40000, $e->getCode(), 'Unexpected error code' );
+        }
+    }
+
+    public function testInvalidCapabilities1() {
+        echo '==testInvalidCapabilities1()';
+        $invalid_capability = array( 'channel0' => array('*', 'publish') );
+        $token_params = array(
+            'capability' => $invalid_capability,
+        );
+        try {
+            $this->ably->request_token(null, $token_params);
+            $this->fail('Invalid capability, expected rejection');
+        } catch (Exception $e) {
+            $this->assertEquals( 40000, $e->getCode(), 'Unexpected error code' );
+        }
+    }
+
+    public function testInvalidCapabilities2() {
+        echo '==testInvalidCapabilities2()';
+        $invalid_capability = array( 'channel0' => array(0) );
+        $token_params = array(
+            'capability' => $invalid_capability,
+        );
+        try {
+            $this->ably->request_token(null, $token_params);
+            $this->fail('Invalid capability, expected rejection');
+        } catch (Exception $e) {
+            $this->assertEquals( 40000, $e->getCode(), 'Unexpected error code' );
+        }
+    }
 
 }
