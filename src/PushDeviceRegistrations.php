85c1829d99c6ac151a9757bfb2d7e6c56386208c
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-10-04 09:50:35 +0200

    New RSH1b1 New push.admin.device_registrations.get

--- src/PushDeviceRegistrations.php
+++ src/PushDeviceRegistrations.php
@@ -29,4 +29,18 @@ class PushDeviceRegistrations {
         return new DeviceDetails ( $body );
     }
 
+    /**
+     *  Returns a DeviceDetails object if the device id is found or results in
+     *  a not found error if the device cannot be found.
+     *
+     *  @param string $deviceId the id of the device
+     */
+    public function get ($deviceId) {
+        $path = '/push/deviceRegistrations/' . $deviceId;
+        $body = $this->ably->get( $path );
+        $body = json_decode(json_encode($body), true); // Convert stdClass to array
+        return new DeviceDetails ( $body );
+    }
+
+
 }
