f5d5fb7b1ad73b756794319149d0f0f8c6ceef6e
sacOO7 <sachinshinde7676@gmail.com>
2022-03-21 10:18:29 +0530

    Added test to verify for platform OS

--- tests/HttpTest.php
+++ tests/HttpTest.php
@@ -5,6 +5,7 @@ use Ably\AblyRest;
 use Ably\Http;
 use Ably\Utils\CurlWrapper;
 use Ably\Models\Untyped;
+use SebastianBergmann\CodeCoverage\Report\PHP;
 
 require_once __DIR__ . '/factories/TestApp.php';
 
@@ -61,7 +62,42 @@ class HttpTest extends \PHPUnit\Framework\TestCase {
      * Verify proper agent header is set as per RSC7d
      */
     public function testAblyAgentHeader() {
+        function getOS($php_os_name) {
+            $os_platform = $php_os_name;
+            $os_array =   array(
+                '/windows nt 10/i'      =>  'Windows 10',
+                '/windows nt 6.3/i'     =>  'Windows 8.1',
+                '/windows nt 6.2/i'     =>  'Windows 8',
+                '/windows nt 6.1/i'     =>  'Windows 7',
+                '/windows nt 6.0/i'     =>  'Windows Vista',
+                '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                '/windows nt 5.1/i'     =>  'Windows XP',
+                '/windows xp/i'         =>  'Windows XP',
+                '/windows nt 5.0/i'     =>  'Windows 2000',
+                '/windows me/i'         =>  'Windows ME',
+                '/win98/i'              =>  'Windows 98',
+                '/win95/i'              =>  'Windows 95',
+                '/win16/i'              =>  'Windows 3.11',
+                '/macintosh|mac os x/i' =>  'Mac OS X',
+                '/mac_powerpc/i'        =>  'Mac OS 9',
+                '/linux/i'              =>  'Linux',
+                '/ubuntu/i'             =>  'Ubuntu',
+                '/iphone/i'             =>  'iPhone',
+                '/ipod/i'               =>  'iPod',
+                '/ipad/i'               =>  'iPad',
+                '/android/i'            =>  'Android',
+                '/blackberry/i'         =>  'BlackBerry',
+                '/webos/i'              =>  'Mobile'
+            );
 
+            foreach ( $os_array as $regex => $value ) {
+                if ( preg_match($regex, $php_os_name ) ) {
+                    $os_platform = $value;
+                }
+            }
+            return $os_platform;
+        }
+        $this->assertEquals("Windows 10", getOs(php_uname('s') . ' ' .php_uname('r')));
     }
 
     /**
