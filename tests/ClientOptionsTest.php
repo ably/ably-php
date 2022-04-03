b10af16386ce2efb9b67a84b7737c1ab71704f6b
sacOO7 <sachinshinde7676@gmail.com>
2022-04-03 10:55:49 +0530

    Added basic test for default options

--- tests/ClientOptionsTest.php
+++ tests/ClientOptionsTest.php
@@ -4,28 +4,16 @@ use Ably\Defaults;
 use Ably\Models\ClientOptions;
 
 class ClientOptionsTest extends \PHPUnit\Framework\TestCase {
-
-    public function testFallbackHosts() {
-        $expectedFallbackHosts = [
-            "a.ably-realtime.com",
-            "b.ably-realtime.com",
-            "c.ably-realtime.com",
-            "d.ably-realtime.com",
-            "e.ably-realtime.com"
-        ];
-        $fallbackHosts = Defaults::$fallbackHosts;
-        $this->assertEquals($expectedFallbackHosts, $fallbackHosts);
-    }
-
-    public function testEnvironmentFallbackHosts() {
-        $expectedFallbackHosts = [
-            "sandbox-a-fallback.ably-realtime.com",
-            "sandbox-b-fallback.ably-realtime.com",
-            "sandbox-c-fallback.ably-realtime.com",
-            "sandbox-d-fallback.ably-realtime.com",
-            "sandbox-e-fallback.ably-realtime.com"
-        ];
-        $fallbackHosts = Defaults::getEnvironmentFallbackHosts("sandbox");
-        $this->assertEquals($expectedFallbackHosts, $fallbackHosts);
+    /**
+     * @testdox RSC15e RSC15g3
+     */
+    public function testWithDefaultOptions() {
+        $clientOptions = new ClientOptions();
+        self::assertEquals('rest.ably.io', $clientOptions->getRestHost());
+        self::assertTrue($clientOptions->tls);
+        self::assertEquals(443, $clientOptions->tlsPort);
+        $fallbackHosts = $clientOptions->getFallbackHosts();
+        sort($fallbackHosts);
+        $this->assertEquals(Defaults::$fallbackHosts, $fallbackHosts);
     }
 }
