e6efe5b0309f19322cd20a82c46581d75a7fd27f
sacOO7 <sachinshinde7676@gmail.com>
2022-04-03 12:24:51 +0530

    Added clientOptions test for production and custom environment

--- tests/ClientOptionsTest.php
+++ tests/ClientOptionsTest.php
@@ -2,7 +2,11 @@
 namespace tests;
 use Ably\Defaults;
 use Ably\Models\ClientOptions;
+use http\Env;
 
+/**
+ * @testdox RSC15b
+ */
 class ClientOptionsTest extends \PHPUnit\Framework\TestCase {
     /**
      * @testdox RSC15e RSC15g3
@@ -16,4 +20,33 @@ class ClientOptionsTest extends \PHPUnit\Framework\TestCase {
         sort($fallbackHosts);
         $this->assertEquals(Defaults::$fallbackHosts, $fallbackHosts);
     }
+
+    /**
+     * @testdox RSC15h
+     */
+    public function testWithProductionEnvironment() {
+        $clientOptions = new ClientOptions();
+        $clientOptions->environment = "Production";
+        self::assertEquals('rest.ably.io', $clientOptions->getRestHost());
+        self::assertTrue($clientOptions->tls);
+        self::assertEquals(443, $clientOptions->tlsPort);
+        $fallbackHosts = $clientOptions->getFallbackHosts();
+        sort($fallbackHosts);
+        $this->assertEquals(Defaults::$fallbackHosts, $fallbackHosts);
+    }
+
+    /**
+     * @testdox RSC15g2 RTC1e
+     */
+    public function testWithCustomEnvironment() {
+        $clientOptions = new ClientOptions();
+        $clientOptions->environment = "sandbox";
+        self::assertEquals('sandbox-rest.ably.io', $clientOptions->getRestHost());
+        self::assertTrue($clientOptions->tls);
+        self::assertEquals(443, $clientOptions->tlsPort);
+        $fallbackHosts = $clientOptions->getFallbackHosts();
+        sort($fallbackHosts);
+        $this->assertEquals(Defaults::getEnvironmentFallbackHosts('sandbox'), $fallbackHosts);
+    }
+
 }
