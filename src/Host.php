a09de17f409e1c1fd01dc59c1f4e41b461eb6cb6
sacOO7 <sachinshinde7676@gmail.com>
2022-04-09 00:54:30 +0530

    Refactored hostcache

--- src/Host.php
+++ src/Host.php
@@ -7,24 +7,21 @@ class Host {
 
 // TODO - Add SyncMutex support to the class to avoid data corruption due to concurrent READ/WRITE (e.g. Apache multithreading environment)
 class HostCache {
-    private $timeoutDuration;
+    private $timeoutDurationInSec;
     private $expireTimeInSec;
     private $host = "";
 
     /**
-     * @param $timeoutDuration
-     * @param $expireTimeInMs
-     * @param string $host
+     * @param $timeoutDurationInMs - $fallbackRetryTimeout in milliseconds
      */
-    public function __construct($timeoutDuration, $expireTimeInMs)
+    public function __construct($timeoutDurationInMs)
     {
-        $this->timeoutDuration = $timeoutDuration;
-        $this->expireTimeInSec = $expireTimeInMs / 1000;
+        $this->timeoutDurationInSec = $timeoutDurationInMs / 1000;
     }
 
     public function put($host) {
         $this->host = $host;
-        $this->expireTimeInSec = time() + $this->timeoutDuration;
+        $this->expireTimeInSec = time() + $this->timeoutDurationInSec;
     }
 
     public function get() {
